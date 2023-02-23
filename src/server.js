afb99bad1c4b8a342f3e9aacb21e3ad0d3020232
EndangSeroz <109642461+EndangSeroz@users.noreply.github.com>
2023-02-23 16:47:49 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-const Hapi = require('@hapi/hapi')
-const routes = require('./routes')
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const server = Hapi.server({
   port: 9000,
@@ -8,15 +8,15 @@ const server = Hapi.server({
   routes: {
     cors: {
       origin: ['*']
-    }
-  }
-})
+    },
+  },
+});
 
 const init = async () => {
-  server.route(routes)
+  server.route(routes);
 
-  await server.start()
-  console.log(`Server berjalan pada ${server.info.uri}`)
-}
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
-init()
+init();
